1103030a9958fcf68f789960c45f4551980213be
jbisbal91 <jbisbal91@gmail.com>
2023-12-18 07:28:28 -0700

    minor fix

--- lib/components/expansion-panel/expansion-panel/expansion-panel.component.d.ts
+++ lib/components/expansion-panel/expansion-panel/expansion-panel.component.d.ts
@@ -8,11 +8,10 @@ export declare class ExpansionPanelComponent implements ExpansionPanel, OnInit {
     expanded: boolean;
     label: string;
     ngxType: 'card' | 'normal';
-    color: string;
     constructor();
     ngOnInit(): void;
     expand(): void;
     guid(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<ExpansionPanelComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<ExpansionPanelComponent, "ngx-expansion-panel", never, { "disabled": { "alias": "disabled"; "required": false; }; "label": { "alias": "label"; "required": false; }; "ngxType": { "alias": "ngxType"; "required": false; }; "color": { "alias": "color"; "required": false; }; }, { "onClick": "onClick"; }, never, ["*"], false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<ExpansionPanelComponent, "ngx-expansion-panel", never, { "disabled": { "alias": "disabled"; "required": false; }; "label": { "alias": "label"; "required": false; }; "ngxType": { "alias": "ngxType"; "required": false; }; }, { "onClick": "onClick"; }, never, ["*"], false, never>;
 }
