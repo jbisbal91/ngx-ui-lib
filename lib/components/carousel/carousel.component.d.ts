2356731d6a6540ee4965fb2c6e3d5baf5d6eaa7a
jbisbal91 <jbisbal91@gmail.com>
2023-12-21 16:41:29 -0700

    auto play

--- lib/components/carousel/carousel.component.d.ts
+++ lib/components/carousel/carousel.component.d.ts
@@ -1,10 +1,17 @@
-import { QueryList } from '@angular/core';
+import { AfterContentInit, QueryList } from '@angular/core';
 import { CarouselItemComponent } from './carousel-item/carousel-item.component';
 import * as i0 from "@angular/core";
-export declare class CarouselComponent {
+export declare class CarouselComponent implements AfterContentInit {
     carouselItems: QueryList<CarouselItemComponent>;
+    ngxAutoPlay: boolean;
+    ngxAutoPlaySpeed: number;
+    currentItem: CarouselItemComponent;
+    constructor();
     ngAfterContentInit(): void;
-    selectItem(carouselItem: CarouselItemComponent): void;
+    autoPlay(index?: number): void;
+    onClick(carouselItem: any): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<CarouselComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CarouselComponent, "ngx-carousel", never, {}, {}, ["carouselItems"], ["*"], false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<CarouselComponent, "ngx-carousel", never, { "ngxAutoPlay": { "alias": "ngxAutoPlay"; "required": false; }; "ngxAutoPlaySpeed": { "alias": "ngxAutoPlaySpeed"; "required": false; }; }, {}, ["carouselItems"], ["*"], false, never>;
+    static ngAcceptInputType_ngxAutoPlay: unknown;
+    static ngAcceptInputType_ngxAutoPlaySpeed: unknown;
 }
